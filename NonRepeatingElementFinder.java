69e9bd739b63f6923e7e61f397b49ebf292e138c
Pratyusha2123 <d.pratyusha21@gmail.com>
2026-09-23 21:30:14 +0530

    add NonRepeatingElementFinder

--- NonRepeatingElementFinder.java
+++ NonRepeatingElementFinder.java
@@ -2,13 +2,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class NonRepeatingElementFinder {
-    public static void main(String[] args) {
-        int[] arr1 = {-1, 2, -1, 3, 2};
-        System.out.println("Output for Example 1: " + firstNonRepeating(arr1));
-        int[] arr2 = {1, 1, 1};
-        System.out.println("Output for Example 2: " + firstNonRepeating(arr2));
-    }
-
     public static int firstNonRepeating(int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : arr) {
@@ -20,5 +13,12 @@ public class NonRepeatingElementFinder {
             }
         }
         return 0;
+    }
+    public static void main(String[] args) {
+
+        int[] arr1 = {-1, 2, -1, 3, 2};
+        System.out.println("Output for Example 1: " + firstNonRepeating(arr1)); 
+        int[] arr2 = {1, 1, 1};
+        System.out.println("Output for Example 2: " + firstNonRepeating(arr2)); 
     }
 }
